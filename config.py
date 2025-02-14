48cb8f278522b995ea8ead3c43a86664423799ef
MarvinMDiaz <88158176+MarvinMDiaz@users.noreply.github.com>
2025-02-13 19:01:52 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import boto3
+
+def get_ssm_parameter(name):
+    """Fetch a parameter from AWS SSM Parameter Store"""
+    ssm = boto3.client("ssm", region_name="us-east-1")  # Adjust to your AWS region
+    response = ssm.get_parameter(Name=name, WithDecryption=True)
+    return response["Parameter"]["Value"]
+
+class Config:
+    SECRET_KEY = get_ssm_parameter("/JBI/SECRET_KEY")
+    RECAPTCHA_SITE_KEY = get_ssm_parameter("/JBI/RECAPTCHA_SITE_KEY")
+    RECAPTCHA_SECRET_KEY = get_ssm_parameter("/JBI/RECAPTCHA_SECRET_KEY")
+    RECIPIENT = get_ssm_parameter("/JBI/RECIPIENT")
+    SES_USERNAME = get_ssm_parameter("/JBI/SES_USERNAME")
+    SES_PASSWORD = get_ssm_parameter("/JBI/SES_PASSWORD")
+    SES_SENDER = get_ssm_parameter("/JBI/SES_EMAIL_SENDER")
